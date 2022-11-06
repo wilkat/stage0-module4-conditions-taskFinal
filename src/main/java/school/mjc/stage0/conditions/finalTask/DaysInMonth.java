048d6da29a2095ca3483afb7ac698947aba599ab
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-06 20:45:17 +0100

    new

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -4,9 +4,22 @@ import java.util.GregorianCalendar;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+       /* switch (month) {
+            case 1: Calendar.JANUARY; break;
+            case 2: Calendar.FEBRUARY; break;
+            case 3: Calendar.MARCH; break;
+            case 4: Calendar.APRIL; break;
+            case 5: Calendar.MAY; break;
+            case 6: Calendar.JUNE; break;
+            case 7: Calendar.JULY; break;
+            case 8: Calendar.AUGUST; break;
+            case 9: Calendar.SEPTEMBER; break;
+            case 10: Calendar.OCTOBER; break;
+            case 11: Calendar.NOVEMBER; break;
+            case 12: Calendar.DECEMBER; break;
+        }*/
         if(year > 0) {
-            Calendar mycal = new GregorianCalendar(year, month,1);
+            Calendar mycal = new GregorianCalendar(year, month-1,1);
             int days = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
             System.out.println(days);
         } else {
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
-        int div = dividend/divider;
+
         if(divider == 0) {
             System.out.println("division by zero");
-        } else if (div*divider == dividend) {
+        } else if ((dividend/divider)*divider == dividend) {
                 System.out.println("can be divided completely");
         } else {
             System.out.println("cannot be divided completely");
