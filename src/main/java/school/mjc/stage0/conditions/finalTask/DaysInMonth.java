032b32b68c804110fd5cd2d4037d6c657a940651
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-06 20:14:03 +0100

    new

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,17 @@
 package school.mjc.stage0.conditions.finalTask;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+
+        if(year > 0) {
+            Calendar mycal = new GregorianCalendar(year, month,1);
+            int days = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
+            System.out.println(days);
+        } else {
+            System.out.println("invalid date");
+        }
 
     }
 }
